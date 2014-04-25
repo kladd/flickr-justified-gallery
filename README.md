772995be910e4cc0a43180af663a9d37a8b81ad2
Kyle Ladd <kyle@laddk.com>
2014-04-25 00:10:53 -0400

    Fix bad, add license, add README

---
+++ README.md
@@ -0,0 +1,34 @@
+## Flickr Justified Gallery
+
+This gallery plugin allows you to create a Flickr photostream-like gallery for your application
+while still using Flickr to host your image files. The plugin use the Flickr API to gather a list
+of recently added public photos for a particular user and displays them in a justified gallery
+using ["Justified Gallery"](http://miromannino.com/projects/justified-gallery/) from Miro Mannino.
+
+## Installation
+
+```bash
+bower install kladd/flickr-justified-gallery
+```
+
+## Usage
+
+Follow the directions on Miro Mannino's project page
+[here](http://miromannino.com/projects/justified-gallery/).
+
+Then replace
+
+```javascript
+$("#mygallery").justifiedGallery();
+```
+
+with
+
+```javascript
+$("#mygallery").flickrJustifiedGallery();
+```
+
+## License
+
+Flickr Justified Gallery is open-sourced software licensed under the
+[MIT license](https://raw.github.com/kladd/flickr-justified-gallery/master/LICENSE)
--- flickrJustifiedGallery.js
+++ flickrJustifiedGallery.js
@@ -1,8 +1,8 @@
-/*!
+/*
  * Flickr Justified Gallery - v0.0.1
  * http://github.com/kladd/flickr-justified-gallery/
- * Copyright (c) 2014 Kyle Ladd
- * Licensed under the MIT license.
+ *
+ * flickrJustifiedGallery.js
  */
 (function($) {
     $.fn.flickrJustifiedGallery = function (arg) {
@@ -15,38 +15,36 @@
             justifiedGallerySettings: undefined
         };
 
-        function checkSettings(context) {
-            if (context.settings.flickrApiKey == "")
+        function checkSettings(settings) {
+            if (settings.flickrApiKey == "")
                 throw 'flickrApiKey must be defined';
 
-            if (context.settings.flickrUserID == "")
+            if (settings.flickrUserID == "")
                 throw 'flickrUserID must be defined';
         }
 
         return this.each(function (index, gallery) {
-            var $gallery= $(gallery);
-            var context = {
-                settings: $.extend({}, defaults, arg)
-            };
+            var settings = $.extend({}, defaults, arg);
+            var $gallery = $(gallery);
 
-            checkSettings(context);
+            checkSettings(settings);
 
             var flickrData = {
-                method: context.settings.flickrApiMethod,
-                api_key: context.settings.flickrApiKey,
-                user_id: context.settings.flickrUserID,
-                per_page: context.settings.flickrPerPage,
+                method: settings.flickrApiMethod,
+                api_key: settings.flickrApiKey,
+                user_id: settings.flickrUserID,
+                per_page: settings.flickrPerPage,
                 format: "json",
                 nojsoncallback: 1
             };
             var flickrRequest = $.ajax({
                 type: "GET",
-                url: context.settings.flickrApiUrl,
+                url: settings.flickrApiUrl,
                 data: flickrData
             });
 
-            flickrRequest.done(function(response)) {
-                for (var i = 0; i < per_page; i++) {
+            flickrRequest.done(function(response) {
+                for (var i = 0; i < settings.flickrPerPage; i++) {
                     var photo = response.photos.photo[i];
                     var url = "http://farm" + photo.farm +
                         ".staticflickr.com/" + photo.server +
@@ -56,12 +54,12 @@
                                 '<img alt="' + title + '" src="' + url +'"/></a>';
                     $gallery.append(html);
                 }
-                $gallery.justifiedGallery(context.settings.justifiedGallerySettings);
-            }
+                $gallery.justifiedGallery(settings.justifiedGallerySettings);
+            });
 
             flickrRequest.fail(function(jqXhr, textStatus) {
                 throw "flickrRequest: " + textStatus;
             });
         });
     }
-}(JQuery));
+}(jQuery));
